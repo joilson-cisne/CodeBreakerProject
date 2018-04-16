241a510a665483fa700c45337d57518e2d3ee322
Joilson Cisne <joilsoncisne@gmail.com>
2018-04-16 18:28:38 -0300

    Call the validateInput function from the guess function

--- src/assets/main.js
+++ src/assets/main.js
@@ -4,9 +4,15 @@ let attempt = document.getElementById('attempt');
 function guess() {
     let input = document.getElementById('user-guess');
     //add functionality to guess function here
+    if (!validateInput(input.value)) {
+        return false;
+    }
+    
     if (!answer.value && !attempt.value) {
         setHiddenFields();
     }
+
+    attempt.value = Number(attempt.value) + 1;
 }
 
 //implement new functions here
